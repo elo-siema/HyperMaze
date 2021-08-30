fd5f48e448ba95d7e10b08600af6d793ed595e7d
= <=>
2021-08-30 19:54:59 +0200

    experiment

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -74,14 +74,31 @@ impl Game {
 
             let numerator = ((x2 - x1) * y1 - (y2 - y1) * x1).abs();
             let denominator = distance_between(x1, y1, x2, y2);//((x2 - x1).powi(2) + (y2 - y1).powi(2)).sqrt();
-            let distance = numerator / denominator;            
+            let distance = numerator / denominator;           
+            
+            let lccollision = line_circle_collision_avg(x1, y1, x2, y2, COLLISION_RADIUS*2.);
+                if let Some (lccolresult) = lccollision {
+                    let lc_distance = distance_between(lccolresult.0, lccolresult.1, 0., 0.);
+                    //println!("lccolresult: distance:{}, x:{}, y:{}",lc_distance, lccolresult.0, lccolresult.1);
+                }
+
+            let distance = match lccollision{
+                Some(lccolresult) => distance_between(lccolresult.0, lccolresult.1, 0., 0.),
+                None => f64::INFINITY
+            };
 
             let helper = distance_between(x1, y1, 0., 0.) + distance_between(x2, y2, 0., 0.) - distance_between(x1, y1, x2, y2);
             let is_between = -EPSILON < helper && helper < EPSILON;
 
-            let collision = distance < COLLISION_RADIUS && is_between;
+            let collision = distance < COLLISION_RADIUS*100. && is_between;
+
+            
+
             if collision { 
                 println!("collision at {}", distance); 
+
+                
+
                 // TODO :: using a normal here is wrong, 
                 // better to find the exact point of collision
                 let normal = Vec2::new((y2-y1) as f32, (x2-x1) as f32);
@@ -115,5 +132,70 @@ impl Game {
             root_ui().label(None, &format!("{}/{} found...", inactive_objects, total_objects));
         }
     }
+}
+
+fn line_circle_collision_avg(x1: f64, y1: f64, x2: f64, y2: f64, r: f64) -> Option<(f64, f64)> {
+    let slope = (y2-y1)/(x2-x1);
+    let intercept = y1 - slope * x1;
+
+    let delta = -4. * (slope.powi(2) + intercept.powi(2) - r.powi(2));
+
+    if(delta < 0.0) { return None } //no collision
+    println!("delta: {}", delta);
+
+    let sqrt_delta = delta.sqrt();
+
+    let result_x1 = (-(2.*slope*intercept) - sqrt_delta) / 2.*(slope.powi(2) + 1.);
+    let result_x2 = (-(2.*slope*intercept) + sqrt_delta) / 2.*(slope.powi(2) + 1.);
+    let result_y1 = slope * result_x1 + intercept;
+    let result_y2 = slope * result_x2 + intercept;
+
+    let result_x = (result_x1 + result_x2) / 2.;
+    let result_y = (result_y1 + result_y2) / 2.;
+
+    Some((result_x, result_y))
+}
+
+fn point_circle_collision(x: f64, y: f64, r: f64) -> bool {
+    if r == 0. { return false }
+    return x*x+y+y <= r*r;
+}
+
+fn line_circle_collision(ax: f64, ay: f64, bx: f64, by: f64, r: f64) -> Option<(f64, f64)> {
+    if point_circle_collision(ax, ay, r) {
+        return Some((ax, ay));
+    }
+    if point_circle_collision(bx, by, r) {
+        return Some((bx, by));
+    }
+
+    //vector d
+    let dx = bx - ax;
+    let dy = by - ay;
+
+    let lcx = -ax;
+    let lcy = -ay;
+
+    let dLen2 = dx*dx + dy*dy;
+    let mut px = dx;
+    let mut py = dy;
+    if dLen2 > 0. {
+        let dp = (lcx * dx + lcy * dy) / dLen2;
+        px *= dp;
+        py *= dp;
+    }
+
+    let resultx = ax + px;
+    let resulty = ay + py;
+
+    let pLen2 = px*px + py*py;
+
+    if point_circle_collision(resultx, resulty, r) 
+        && pLen2 <= dLen2 
+        && (px * dx + py * dy) >= 0. {
+            return Some((resultx, resulty))
+        }
+
+    None
 }
 
